545a4896cb761d4bd9a71d71adad157339e3ebee
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2019-03-13 12:19:53 -0300

    otimizations

---
+++ VIOLA_1S_1P_CFC.py
@@ -0,0 +1,33 @@
+import scipy.io
+from solverFd import solverfd
+from SolverResult import SolverResult
+
+# Input parameters
+
+# String
+string_parameters = scipy.io.loadmat('one_string_1.mat', squeeze_me=True)
+string_parameters = string_parameters['string_parameters']
+
+# Body
+body_matrix = scipy.io.loadmat("Viola_ComplexModes_Yzz_Yyz_NoNorm_matrix.mat", squeeze_me=True)
+body_matrix = body_matrix['body_matrix']
+
+# Pluck parameters
+pluck_parameters = scipy.io.loadmat("pluck_parameters_1.mat", squeeze_me=True)
+pluck_parameters = pluck_parameters['pluck_parameters']
+
+# String Matrix
+string_matrix = scipy.io.loadmat("one_string_1_matrix.mat", squeeze_me=True)
+string_matrix = string_matrix['string_matrix']
+
+# Simulation parameters
+fhmax = 20000  # Maximum frequency of string
+Nb = 36
+Fs = 5*44100
+dt = 1/Fs
+d = 8  # duration
+
+# Resolution
+
+result = solverfd(body_matrix, string_matrix, string_parameters, pluck_parameters, d, Fs)
+
--- solverFd.py
+++ solverFd.py
@@ -1,9 +1,10 @@
 import numpy as np
-import math
+import time
 from SolverResult import SolverResult
 
 
 def solverfd(body_matrix, string_matrix, string_parameters, pluck_parameters, d, Fs):
+    start = time.time()
     # Simulation parameters
     dt = 1 / Fs
     t = np.linspace(0, np.floor(d / dt) - 1, np.floor(d / dt)) * dt
@@ -42,27 +43,27 @@ def solverfd(body_matrix, string_matrix, string_parameters, pluck_parameters, d,
 
     # Modal quantities
 
-    an = np.zeros((Ns+1, len(t)))
-    bn = np.zeros((Nb, len(t)))
+    an = np.zeros((Ns+1, len(t)), dtype=np.float64)
+    bn = np.zeros((Nb, len(t)), dtype=np.float64)
     an[:, (1, 2)] = 0
     bn[:, (1, 2)] = 0
 
     # Physical quantity
-    Fc = np.zeros((1, len(t)))
+    Fc = np.zeros((1, len(t)), dtype=np.float64)
     Fc[:, (1, 2)] = 0
 
     # Finite difference scheme
 
     # String
     AI = np.linalg.inv(MJ / dt ** 2 + CJ / (2 * dt))
-    A1 = AI * (2 * MJ / dt ** 2 - KJ)
-    A2 = AI * (CJ / (2 * dt) - MJ / dt ** 2)
+    A1 = AI @ (2 * MJ / dt ** 2 - KJ)
+    A2 = AI @ (CJ / (2 * dt) - MJ / dt ** 2)
     A3 = AI
 
     # Body
     BI = np.linalg.inv(MK / dt ** 2 + CK / (2 * dt))
-    B1 = BI * (2 * MK / dt ** 2 - KK)
-    B2 = BI * (CK / (2 * dt) - MK / dt ** 2)
+    B1 = BI @ (2 * MK / dt ** 2 - KK)
+    B2 = BI @ (CK / (2 * dt) - MK / dt ** 2)
     B3 = BI
 
     # Evaluation of string mode shapes
@@ -78,25 +79,44 @@ def solverfd(body_matrix, string_matrix, string_parameters, pluck_parameters, d,
         PhiSe[i] = np.sin(j[i-1] * np.pi * xp / L)
 
     # ITERATIVE COMPUTATION OF SOLUTION
+    Fc = np.zeros(len(t))
+    Fc1 = -(1 / (PhiSc @ A3 @ PhiSc.T + PhiB @ B3 @ PhiB.T))
+    PhiBB1 = PhiB @ B1
+    PhiBB2 = PhiB @ B2
+    PhiScA1 = PhiSc @ A1
+    PhiScA2 = PhiSc @ A2
+    PhiScA3PhiSeT = PhiSc @ A3 @ PhiSe.T
+    A3PhiSeT = A3 @ PhiSe.T
+    A3PhiScT = A3 @ PhiSc.T
+    B3PhiBT = B3 @ PhiB.T
+    Fc2 = np.float64(0)
+    Fe = np.float64(0)
 
     for i in range(1, len(t)-1):
 
         Fe = (F0 / dp) * (i * dt - Ti) * (np.heaviside((i * dt - Ti), 0.5) - np.heaviside((i * dt - Tr), 0.5))
 
-        Fc[0, i] = -(1 / (PhiSc * A3 * PhiSc.T + PhiB * B3 * PhiB.T)) * (PhiB * B1 * bn[:, i] + PhiB * B2 * bn[:, i - 1] - PhiSc * A1 * an[:, i] - PhiSc * A2 * an[:, i-1] - PhiSc * A3 * PhiSe.T * Fe)
+        Fc2 = (PhiBB1 @ bn[:, i] + PhiBB2 @ bn[:, i - 1] - PhiScA1 @ an[:, i] - PhiScA2 @ an[:, i-1] - PhiScA3PhiSeT * Fe)
+        Fc[i] = Fc1 * Fc2
+
+        '''print('i: {}   Fe: {}   Fc: {}   Fc1: {}   Fc2: {}'.format(i, Fe, Fc[i], Fc1, Fc2))'''
 
         # compute modal coordinates for next time - step
 
-        an[:, i + 1] = A1 * an[:, i] + A2 * an[:, i - 1] + A3 * PhiSe.T * Fe - A3 * PhiSc.T * Fc[1, i]
+        an[:, i + 1] = A1 @ an[:, i] + A2 @ an[:, i - 1] + A3PhiSeT * Fe - A3PhiScT * Fc[i]
 
-        bn[:, i + 1] = B1 * bn[:, i] + B2 * bn[:, i - 1] + B3 * PhiB.T * Fc[1, i]
+        bn[:, i + 1] = B1 @ bn[:, i] + B2 @ bn[:, i - 1] + B3PhiBT * Fc[i]
+
+        if i % 1000 == 0:
+            print("Progress: {} of {}".format(i, len(t)))
 
     # String displacement
-
-    z_p = np.real(PhiSe*an)*1e3  # in mm
-    z_b1 = np.real(PhiSc*an)*1e3  # in mm
+    end = time.time()
+    print(end-start)
+    z_p = np.real(PhiSe @ an)*1e3  # in mm
+    z_b1 = np.real(PhiSc @ an)*1e3  # in mm
 
     # Body displacement
-    z_b2 = np.real(PhiB*bn)*1e3  # in mm
+    z_b2 = np.real(PhiB @ bn)*1e3  # in mm
 
     return SolverResult(z_p, z_b1, z_b2, an, bn, Fc)
