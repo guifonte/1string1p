38cdf0f73ebe227d49daef04b261b1788c680f51
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2019-04-21 21:11:40 -0300

    organize the folder structure

--- VIOLA_1S_1P_CFC.py
+++ VIOLA_1S_1P_CFC.py
@@ -1,36 +1,39 @@
 import librosa
 
 import time
-from strings import stringscalculator, StringParameters
-from body import BodyMatrix
-from pluck import PluckParameters
-from solver import solverfd
+from src.strings import stringscalculator, StringParameters
+from src.body import BodyMatrix
+from src.pluck import PluckParameters
+from src.solver.solver import solverfd
+
 # Input parameters
-
-# String
-string_parameters = StringParameters.frommat('one_string_1.mat')
-
-# Body
-# body_matrix = BodyMatrix.frommat("Viola_ComplexModes_Yzz_Yyz_NoNorm_matrix.mat")
-body_matrix = BodyMatrix.fromnp("Ortho_plate_fmk", "Ortho_plate_phi")
-
-# Pluck parameters
-# pluck_parameters = PluckParameters(0.50, 0.001, 0.008, 1, 0)
-pluck_parameters = PluckParameters.frommat("pluck_parameters_1.mat")
+stringpath = './DATA/STRINGS/one_string_1.mat'
+bodyfmkpath = "./DATA/BODY/Ortho_plate_fmk"
+bodyphipath = "./DATA/BODY/Ortho_plate_phi"
+pluckpath = "./DATA/PLUCK/pluck_parameters_1.mat"
 
 # Simulation parameters
 fhmax = 20000  # Maximum frequency of string
 Nb = 36
 Fs = 5*44100
 dt = 1/Fs
-d = 8  # duration
+d = 4  # duration
 
+# String
+string_parameters = StringParameters.frommat(stringpath)
 string_matrix = stringscalculator(string_parameters, fhmax)
 
-# Resolution
+# Body
+# body_matrix = BodyMatrix.frommat("./DATA/BODY/Viola_ComplexModes_Yzz_Yyz_NoNorm_matrix.mat")
+body_matrix = BodyMatrix.fromnp(bodyfmkpath, bodyphipath)
 
+# Pluck parameters
+# pluck_parameters = PluckParameters(0.50, 0.001, 0.008, 1, 0)
+pluck_parameters = PluckParameters.frommat(pluckpath)
+
+# Resolution
 result = solverfd(body_matrix, string_matrix, pluck_parameters, d, Fs)
 
 timestamp = str(time.time()).split('.')[0]
-librosa.output.write_wav('./out_'+timestamp+'.wav', result.z_p, sr=44100, norm=True)
+librosa.output.write_wav('./OUT/out_'+timestamp+'.wav', result.z_p, sr=44100, norm=True)
 
--- src/solver/csolver.pyx
+++ src/solver/csolver.pyx
@@ -3,4 +3,9 @@ import numpy as np
 
 class CSolverResult(object):
     def __init__(self, an, bn, Fc)
+        self.an = an
+        self.bn = bn
+        self.Fc = Fc
+
+cdef csolverinteration(
     
