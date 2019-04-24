cabfa2e64d94853fdf73f28526247d155a758116
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2019-04-24 02:59:51 -0300

    adapted solver for generic parameter for 1p

--- src/solver/solver.py
+++ src/solver/solver.py
@@ -311,3 +311,125 @@ def solverfd2p(body_matrix, string_matrix, pluck_parameters, d, Fs):
     y_b2 = np.real(PHIB[2, :] @ b) * 1e3  # in mm
 
     return SolverResult2p(z_p, y_p, z_b1, z_b2, y_b1, y_b2, a, b, Fc)
+
+
+def solverfd_1(body_matrix, string_matrix, pluck_parameters, d, Fs):
+    start = time.time()
+    # Simulation parameters
+    dt = 1 / Fs
+    t = np.linspace(0, np.floor(d / dt) - 1, np.floor(d / dt)) * dt
+
+    # Input parameters
+
+    # String
+    A1 = string_matrix.A1
+    A2 = string_matrix.A2
+    GSe = string_matrix.GSe
+    GSc = string_matrix.GSc
+    PhiSe = string_matrix.PhiSe
+    PhiSc = string_matrix.PhiSc
+    L = string_matrix.L
+    Ns = string_matrix.Ns
+
+    j = np.linspace(1, Ns, Ns)
+
+    # Body
+
+    B1 = body_matrix.B1
+    B2 = body_matrix.B2
+    GBz = body_matrix.GBz
+    PhiBz = body_matrix.PhiBz
+    GBy = body_matrix.GBy
+    PhiBy = body_matrix.PhiBy
+    Nb = body_matrix.Nb
+    xb = L
+
+    # Pluck parameters
+
+    xp = pluck_parameters.xp
+    Ti = pluck_parameters.Ti
+    dp = pluck_parameters.dp
+    F0 = pluck_parameters.F0
+    Tr = Ti + dp
+
+    # Initialisation
+
+    # Modal quantities
+
+    anf = np.ones((Ns + 1, int(len(t)/5)), dtype=np.float64)
+    bnf = np.ones((Nb, int(len(t)/5)), dtype=np.float64)
+
+    anf[:, 0] = 0
+    bnf[:, 0] = 0
+    ana = np.zeros(Ns + 1, dtype=np.float64)
+    anb = np.zeros(Ns + 1, dtype=np.float64)
+    bna = np.zeros(Nb, dtype=np.float64)
+    bnb = np.zeros(Nb, dtype=np.float64)
+
+    # Physical quantity
+    Fc = np.zeros((1, len(t)), dtype=np.float64)
+    Fc[:, (1, 2)] = 0
+
+    # ITERATIVE COMPUTATION OF SOLUTION
+
+    Fc1 = -(1 / (PhiSc @ GSc + PhiBz @ PhiBz))
+    PhiBB1 = PhiBz @ B1
+    PhiBB2 = PhiBz @ B2
+    PhiScA1 = PhiSc @ A1
+    PhiScA2 = PhiSc @ A2
+    PhiScGse = PhiSc @ GSe
+    F0_dp = F0/dp
+    B1_diag = np.diagonal(B1)
+    B2_diag = np.diagonal(B2)
+
+    Fcf = np.zeros(int(len(t) / 5))
+
+    for i in range(1, len(t)-1):
+
+        Fe = F0_dp * (i * dt - Ti) * (np.heaviside((i * dt - Ti), 0.5) - np.heaviside((i * dt - Tr), 0.5))
+
+        # Fc2 = (PhiBB1@bn[:, i] + PhiBB2@bn[:, i - 1] - PhiScA1@an[:, i] - PhiScA2@an[:, i-1] - PhiScA3PhiSeT*Fe)
+
+        # Fc[i] = Fc1 * Fc2
+
+        # compute modal coordinates for next time - step
+
+        # an[:, i + 1] = A1 @ an[:, i] + A2 @ an[:, i - 1] + A3PhiSeT * Fe - A3PhiScT * Fc[i]
+
+        # bn[:, i + 1] = B1_diag * bn[:, i] + B2_diag * bn[:, i - 1] + B3PhiBT * Fc[i]
+
+        if i % 2 == 0:
+            Fc2temp = (PhiBB1 @ bna + PhiBB2 @ bnb - PhiScA1 @ ana - PhiScA2 @ anb - PhiScGse * Fe)
+            Fctemp = Fc1 * Fc2temp
+            anb = A1 @ ana + A2 @ anb + GSe * Fe - GSc * Fctemp
+            bnb = B1_diag * bna + B2_diag * bnb + GBz * Fctemp
+        else:
+            Fc2temp = (PhiBB1 @ bnb + PhiBB2 @ bna - PhiScA1 @ anb - PhiScA2 @ ana - PhiScGse * Fe)
+            Fctemp = Fc1 * Fc2temp
+            ana = A1 @ anb + A2 @ ana + GSe * Fe - GSc * Fctemp
+            bna = B1_diag * bnb + B2_diag * bna + GBz * Fctemp
+
+        if i % 5 == 0:
+            if i % 2 == 0:
+                anf[:, int(i/5)] = anb
+                bnf[:, int(i/5)] = bnb
+            else:
+                anf[:, int(i/5)] = ana
+                bnf[:, int(i/5)] = bna
+
+            Fcf[int(i/5)] = Fctemp
+
+        if i % 20000 == 0:
+            temp_end = time.time() - start
+            print("Progress: {a:.2f}% - {b:.2f}s".format(a=((float(i) / len(t)) * 100), b=temp_end))
+
+    # String displacement
+    end = time.time()
+    print("Elapsed time = {:.2f}s".format(end - start))
+    z_p = np.real(PhiSe @ anf)*1e3  # in mm
+    z_b1 = np.real(PhiSc @ anf)*1e3  # in mm
+
+    # Body displacement
+    z_b2 = np.real(PhiBz @ bnf)*1e3  # in mm
+
+    return SolverResult1p(z_p, z_b1, z_b2, anf, bnf, Fcf)
